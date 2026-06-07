60e852699d6e76c7cd730615b77748ec3aafa0ca
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-06-07 09:50:51 +0530

    Create Solution.java

---
+++ Easy/Strings/2716. Minimize String Length/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        for(char c : s.toCharArray()){
+            set.add(c);
+        }
+
+        return set.size();
+    }
+}

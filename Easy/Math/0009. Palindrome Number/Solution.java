3d6478342fce3fc03d670c691d8be6fdb5eb3ed6
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-06-20 09:49:53 +0530

    Create Solution.java

---
+++ Easy/Math/0009. Palindrome Number/Solution.java
@@ -0,0 +1,16 @@
+class Solution { 
+    public boolean isPalindrome(int x) { 
+        if(x < 0 || (x % 10 == 0 && x != 0)) 
+            return false; 
+
+        int revHalf = 0; 
+
+        while(x > revHalf){ 
+            int rem = x % 10; 
+            revHalf = revHalf * 10 + rem; 
+            x /= 10; 
+        } 
+        
+        return x == revHalf || x == revHalf / 10; 
+    } 
+}

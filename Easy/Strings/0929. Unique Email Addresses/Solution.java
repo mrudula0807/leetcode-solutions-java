76c4661ff7bda3af96c1a6cefd27de1d78779e3b
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-06-20 16:36:37 +0530

    Create Solution.java

---
+++ Easy/Strings/0929. Unique Email Addresses/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for(String email : emails) {
+
+            int at = email.indexOf('@');
+
+            String local = email.substring(0, at);
+            String domain = email.substring(at);
+
+            int plus = local.indexOf('+');
+
+            if(plus != -1){
+                local = local.substring(0, plus);
+            }
+
+            local = local.replace(".", "");
+            
+            set.add(local + domain);
+        }
+
+        return set.size();
+    }
+}

0a2f455e0272b4a1bafaef6699a1bb66bc93770b
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-07-10 09:48:11 +0530

    Create Solution.java

---
+++ Easy/Math/2160. Minimum Sum of Four Digit Number After Splitting Digits/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        
+        for(int i = 0; i < 4; i++) {
+            arr[i] = num % 10;
+            num /= 10;
+        }
+
+        Arrays.sort(arr);
+
+        int num1 = arr[0] * 10 + arr[2];
+        int num2 = arr[1] * 10 + arr[3];
+
+        return num1 + num2;        
+    }
+}

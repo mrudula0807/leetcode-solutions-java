1e1f28b57b14fb9d878d7d1c51aa2641227c17e0
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-07-11 14:51:25 +0530

    Create Solution.java

---
+++ Easy/Strings/0942. DI String Match/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        
+        int n = s.length();
+
+        int[] res = new int[n + 1];
+
+        int low = 0;
+        int high = n;
+
+        for(int i = 0; i < n; i++){
+
+            if(s.charAt(i) == 'I'){
+                res[i] = low++;
+            } else {
+                res[i] = high--;
+            }
+        }
+
+        res[n] = low;
+
+        return res;    
+    }
+}

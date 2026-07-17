9731296a11a8ec579771e91b91d191d11868064f
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-07-17 15:59:58 +0530

    Create Solution.java

---
+++ Easy/Math/2485. Find the Pivot Integer/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int pivotInteger(int n) {
+        
+        int total = n * (n + 1) / 2;
+
+        int x = (int) Math.sqrt(total);
+
+        return x * x == total ? x : -1;
+    }
+}

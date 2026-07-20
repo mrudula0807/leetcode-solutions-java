6d65936d3c5a6bf7b4ff077c076d74080bb0f4d2
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-07-20 16:56:19 +0530

    Create Solution.java

---
+++ Easy/Math/3099. Harshad Number/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+
+        int sum = 0;
+        int temp = x;
+
+        while(temp > 0){
+
+            int digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+        
+        return x % sum == 0 ? sum : -1;
+    }
+}
